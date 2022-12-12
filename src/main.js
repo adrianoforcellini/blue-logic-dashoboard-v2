6b051eb88a89df2d1c3c760953b168b53af32077
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-12-12 12:22:20 -0300

    bootstrap vue 3

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(BootstrapVue3)
 
 app.mount('#app')
